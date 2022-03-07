d0e85de759de2ed0aac55a27abfae319224e36cf
Igor Leal <igorhenriqueleal3@gmail.com>
2022-03-07 16:10:24 -0300

    feat: Requisito 2 concluído.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,53 +1,76 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
 import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
-/*  constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
-      pageLogin: '',
-      loginSubmitButton: 'disabled',
+      name: '',
+      buttonDisabled: true,
+      loading: false,
+      loggedIn: false,
     };
   }
- */
-  handleChangeOnClick = (target) => {
-    const { name } = target;
-    const value = target.disabled === 'disabled' ? 'disabled' : 'enabled';
 
-    this.setState({ [name]: value });
+  handleChange = ({ target }) => {
+    const { value } = target;
+    const minLengthInput = 3;
+
+    this.setState(() => ({
+      name: value,
+      buttonDisabled: value.length < minLengthInput,
+    }));
+  }
+
+  loginChangeSubmit = (event) => {
+    const { name } = this.state;
+    event.preventDefault();
+
+    this.setState({
+      loading: true,
+    },
+    async () => {
+      await createUser({ name });
+      this.setState({
+        loading: false,
+        loggedIn: true,
+      });
+    });
   }
 
   render() {
-    const { pageLogin } = this.props;
+    const { buttonDisabled, loading, loggedIn } = this.state;
     return (
       <div data-testid="page-login">
-        <form>
-          <input
-            data-testid="login-name-input"
-            type="text"
-            name="pageLogin"
-            value={ pageLogin }
-            id="page-login"
-          />
-          <button
-            data-testid="login-submit-button"
-            type="button"
-            name="loginSubmitButton"
-            id="login-submit-button"
-            disabled="disabled"
-            onClick={ () => createUser({ name: '' }) }
-            onChange={ this.handleChangeOnClick }
-          >
-            Entrar
-          </button>
-        </form>
+        {
+          (loading === true)
+            ? (<Loading />)
+            : (
+              <form>
+                <input
+                  data-testid="login-name-input"
+                  type="text"
+                  onChange={ this.handleChange }
+                />
+                <button
+                  data-testid="login-submit-button"
+                  type="button"
+                  disabled={ buttonDisabled }
+                  onClick={ this.loginChangeSubmit }
+                >
+                  Entrar
+                </button>
+              </form>
+            )
+        }
+        {loggedIn && <Redirect to="search" />}
       </div>
     );
   }
 }
 
-Login.propTypes = {
-  pageLogin: PropTypes.string.isRequired,
-};
+// ----- Referências -----
+// REdirect: https://v5.reactrouter.com/web/api/Redirect
