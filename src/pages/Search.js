75e630dcd59fcfdcbb79d37a425768d65e89a0ee
Igor Leal <igorhenriqueleal3@gmail.com>
2022-03-07 19:10:43 -0300

    feat: Requisito 5 iniciado.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+/* import searchAlbumsAPI from '../services/searchAlbumsAPI'; */
 import Header from '../components.js/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      buttonDisabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    const minLengthInput = 2;
+
+    this.setState(() => ({
+      buttonDisabled: value.length < minLengthInput,
+    }));
+  }
+
   render() {
+    const { buttonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+          />
+
+          <button
+            type="button"
+            testid="search-artist-button"
+            disabled={ buttonDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
