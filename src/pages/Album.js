b7716f59453a63ac06854edfcb6239ca1a6bde97
Igor Leal <igorhenriqueleal3@gmail.com>
2022-03-10 08:26:43 -0300

    Refazendo o Requisito 7.

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,20 +1,27 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Header from '../components.js/Header';
 import getMusics from '../services/musicsAPI';
 import Loading from './Loading';
+import MusicCard from '../components.js/MusicCard';
 
 export default class Album extends Component {
   constructor() {
     super();
 
     this.state = {
-      loading: false,
-      collectionId: '',
+      loading: true,
       searchMusics: [],
     };
   }
 
-  handleChange = ({ target }) => {
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const result = await getMusics(id);
+    console.log(result);
+  }
+
+  /* handleChange = ({ target }) => {
     const { value } = target;
 
     this.setState({
@@ -37,7 +44,7 @@ export default class Album extends Component {
       });
     });
   }
-
+ */
   render() {
     const { loading, searchMusics } = this.state;
     return (
@@ -59,3 +66,19 @@ export default class Album extends Component {
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+// ----- Referências: -----
+// match.params:
+//      1. https://pt.stackoverflow.com/questions/337407/passar-match-params-para-dentro-de-outro-component
+//      2. https://www.oreilly.com/library/view/react-router-quick/9781789532555/88981f68-f90b-41e4-b483-747f15587c81.xhtml
+//      3. https://v5.reactrouter.com/web/api/match
+
+// componentDidMount: https://github.com/luacomacento/revisao-react/blob/main/src/App.js
