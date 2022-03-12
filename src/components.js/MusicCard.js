a698d8b647f24eaf028185d12941f874786452c1
Igor Leal <igorhenriqueleal3@gmail.com>
2022-03-12 10:04:22 -0300

    Requisito 9 falhando em um teste e Requisito 10 concluído.

--- src/components.js/MusicCard.js
+++ src/components.js/MusicCard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Loading from '../pages/Loading';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 export default class MusicCard extends Component {
   constructor() {
@@ -9,21 +9,32 @@ export default class MusicCard extends Component {
 
     this.state = {
       loading: false,
-      checked: false,
+      check: false,
     };
+  }
+
+  async componentDidMount() {
+    const { trackId } = this.props;
+    const favoritesMusics = await getFavoriteSongs();
+
+    this.setState({
+      check: favoritesMusics.some((favorite) => favorite.trackId === trackId),
+    });
   }
 
   // Usar a mesma lógica do component Login para essa função
   // Concluído na mentoria com ajuda do Braddock e dos colegas André Alves e Hugo Mafra - Turma 19A
-  handleChange = ({ target }) => {
+  handleChange = ({ target }, id) => {
     const { checked } = target;
-    const { id } = this.props;
+
     this.setState({
       loading: true,
-      checked,
+      check: checked,
     },
     async () => {
-      await addSong(id);
+      if (checked) {
+        await addSong(id);
+      }
       this.setState({
         loading: false,
       });
@@ -31,7 +42,7 @@ export default class MusicCard extends Component {
   }
 
   render() {
-    const { loading, checked } = this.state;
+    const { loading, check } = this.state;
     const { music } = this.props;
     const { trackName, previewUrl, trackId } = music;
 
@@ -60,7 +71,7 @@ export default class MusicCard extends Component {
                     data-testid={ `checkbox-music-${trackId}` }
                     name="favorite-song"
                     onChange={ this.handleChange }
-                    checked={ checked }
+                    checked={ check }
                   />
                 </label>
               </>
@@ -77,5 +88,5 @@ MusicCard.propTypes = {
     previewUrl: PropTypes.string,
     trackId: PropTypes.number,
   }).isRequired,
-  id: PropTypes.string.isRequired,
+  trackId: PropTypes.string.isRequired,
 };
